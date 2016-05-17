acf533106205fa9f9f662c359baa27fae7555a82
Andrew Faraday <andrewfaraday@hotmail.com>
2016-05-17 12:40:40 +0000

    Some mobile event tuning

--- public/script/tdp/commands/touch_screen_events.js
+++ public/script/tdp/commands/touch_screen_events.js
@@ -5,7 +5,7 @@ TDP.commands.touchScreenEvents = {
     'swipeleft': 'moveLeft',
     'swipedown': 'moveDown',
     'swipeup': 'moveUp',
-    'tap': 'specialAttack'
+    'taphold': 'specialAttack'
   },
 
   init: function () {
@@ -14,16 +14,15 @@ TDP.commands.touchScreenEvents = {
         Object.keys(this.event_mappings),
         function (i, event_name) {
           var action_name = TDP.commands.touchScreenEvents.event_mappings[event_name];
-          console.log(event_name);
-          console.log(action_name);
           $(document).on(
             event_name,
-            'body',
+            '.TDP_board_inner, #TDP_readout',
             function (e) {
               e.preventDefault();
               TDP.player[action_name]();
             }
           );
+
         }
       );
     }
