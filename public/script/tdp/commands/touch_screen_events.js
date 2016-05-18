79ceb5629befc60b7218f3516b665f7bcd82d886
Andrew Faraday <andrewfaraday@hotmail.com>
2016-05-18 21:56:52 +0100

    Disabled selection from mobile, on mobile, and for readout too

--- public/script/tdp/commands/touch_screen_events.js
+++ public/script/tdp/commands/touch_screen_events.js
@@ -8,44 +8,63 @@ TDP.commands.touchScreenEvents = {
     //'taphold': 'specialAttack'
   },
 
+  initSpecialAttack: function () {
+    $(document).on(
+      'taphold',
+      '.TDP_board_inner, #TDP_readout',
+      function (e) {
+        e.preventDefault();
+        if (!TDP.commands.touchScreenEvents.suppressSpecial) {
+          TDP.player.specialAttack();
+        }
+      }
+    );
+  }, 
+  
+  initNamedActions: function () {
+    $.each(
+      Object.keys(this.event_mappings),
+      function (i, event_name) {
+        var action_name = TDP.commands.touchScreenEvents.event_mappings[event_name];
+        $(document).on(
+          event_name,
+          '.TDP_board_inner, #TDP_readout',
+          function (e) {
+            e.preventDefault();
+            TDP.player[action_name]();
+            TDP.commands.touchScreenEvents.suppressSpecial = true;
+            setTimeout(
+              function () {
+                TDP.commands.touchScreenEvents.suppressSpecial = false;
+              },
+              1000
+            )
+          }
+        );
+      }
+    );
+  }, 
+  
+  configureMobileEvents: function () {
+    $.event.special.tap.tapholdThreshold = 1000;
+  }, 
+  
   init: function () {
     if (this.onMobile()) {
-      $.each(
-        Object.keys(this.event_mappings),
-        function (i, event_name) {
-          var action_name = TDP.commands.touchScreenEvents.event_mappings[event_name];
-          $(document).on(
-            event_name,
-            '.TDP_board_inner, #TDP_readout',
-            function (e) {
-              e.preventDefault();
-              TDP.player[action_name]();
-              TDP.commands.touchScreenEvents.suppressSpecial=true;
-              setTimeout(
-                function(){
-                  TDP.commands.touchScreenEvents.suppressSpecial=false;
-                },
-                1000
-              )
-            }
-          );
-        }
-      );
-      $(document).on(
-        'taphold',
-        '.TDP_board_inner, #TDP_readout',
-        function (e) {
-          e.preventDefault();
-          if (!TDP.commands.touchScreenEvents.suppressSpecial) {
-            TDP.player.specialAttack();
-          }
-        }
-      );
-
-      $.event.special.tap.tapholdThreshold = 1000;
+      this.initNamedActions();
+      this.initSpecialAttack();
+      this.configureMobileEvents();
+      this.disableSelection()
     }
   },
 
+  disableSelection: function() {
+    $('.TDP_control_bar_button, .TDP_board, #TDP_readout')
+      .attr('unselectable', 'on')
+      .css('user-select', 'none')
+      .on('selectstart', false);
+  },
+  
   onMobile: function () {
     return $.support.touch;
   }
